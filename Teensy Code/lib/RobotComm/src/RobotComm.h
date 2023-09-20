a3e4d5d62b31aa093afb6c794aa0c9780958b943
Erick Nunez <ercknz@gmail.com>
2023-09-20 14:07:27 -0400

    updated

--- Teensy Code/lib/RobotComm/src/RobotComm.h
+++ Teensy Code/lib/RobotComm/src/RobotComm.h
@@ -33,9 +33,23 @@ class RobotComm {
   protected:
 
     const int _BAUDRATE;
-    const byte _READ_HEADER[4] = {170, 6, 9, 69};
-    const byte _WRITE_HEADER[4] = {};
+    const byte _READ_HEADER[4]  = {170,  6, 9,  69};
+    const byte _WRITE_HEADER[4] = {150, 10, 1, 101};
+    const int16_t _presQ_SLOT = 8;
+    const int16_t _presQDOT_SLOT = 20;
+    const int16_t _presCURRENT_SLOT = 32;
+    const int16_t _presXYZ_SLOT = 44;
+    const int16_t _presXYZdot_SLOT = 56;
+    const int16_t _BLANK_SLOT = 68;
 
+    float qPres_M[3], qDotPres_M[3];
+    float xyzPres_M[3], xyzDotPres_M[3];
+    float xyzGoal_M[3], xyzDotGoal_M[3];
+    float presCurrent_M[3];
+    float   springF_M;
+    float   scalingFactor_M;
+
+    int16_t torqueMode;
 
     const float  _A1A2,      _L1,        _L2;
     const double _OFFSET,    _PHI,       _H_OF_L2;
@@ -44,14 +58,7 @@ class RobotComm {
     const double _Q4_MIN,    _Q4_MAX;
     const double _INNER_R,   _Z_LIMIT;
     const double _SPRING_Li, _BETAi, _SPRING_Fi;
-    int32_t qPresCts_M[3],  qDotPresCts_M[3];
-    float   qPres_M[3],     qDotPres_M[3];
-    float   xyzPres_M[3],   xyzDotPres_M[3];
-    int32_t qCts_M[3],      qDotCts_M[3];
-    float   q_M[3],         qDot_M[3];
-    float   xyz_M[3],       xyzDot_M[3];
-    float   springF_M;
-    float   scalingFactor_M;
+    
 };
 
 #endif // ROBOT_COMM_H
